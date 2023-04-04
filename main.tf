29e0e43c47e5c2d9296a1f0825bc81198a7c648b
Russell Schaaf <81342619+schaafru@users.noreply.github.com>
2023-04-04 13:22:36 -0700

    autoscaling

--- main.tf
+++ main.tf
@@ -29,18 +29,23 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.9.0"
+  
+  name     = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_group      = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-
-  tags = {
-    Name = "Learning terraform"
-  }
 }
 
-module "blog-alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
